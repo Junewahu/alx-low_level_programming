646472a3a4e8c783120f604bb9bc5f8cc3832cfc
Junewahu <junewahu@gmail.com>
2023-02-21 13:27:54 -0800

    1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,35 +1,30 @@
 #ifndef main_h
+#define main_h
 
-        #define main_h
+int _putchar(char);
 
+void print_alphabet(void);
 
+void print_alphabet_x10(void);
 
-        int _putchar(char);
+int _islower(int c);
 
-        void print_alphabet(void);
+int _isalpha(int c);
 
-        void print_alphabet_x10(void);
+int print_sign(int n);
 
-        int _islower(int c);
+int _abs(int);
 
-        int _isalpha(int c);
+int print_last_digit(int);
 
-        int print_sign(int n);
+void jack_bauer(void);
 
-        int _abs(int);
+void times_table(void);
 
-        int print_last_digit(int);
+int add(int, int);
 
-        void jack_bauer(void);
+void print_to_98(int n);
 
-        void times_table(void);
+void print_times_table(int n);
 
-        int add(int, int);
-
-        void print_to_98(int n);
-
-        void print_times_table(int n);
-
-
-
-        #endif
+#endif
