09a256c3b2682ccda8abf83eef8ede0c3c8f7216
Junewahu <junewahu@gmail.com>
2023-02-22 01:49:03 -0800

    10-11

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,30 +2,18 @@
 
 
 
-        /**
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
 
-         * add - adds two integers and returns the result
+int add(int a, int b)
 
-         * @a: int to be added to b
+{
+	int c;
 
-         * @b: int to be added to a
-
-         * Return: sum of a and b
-
-         */
-
-        int add(int a, int b)
-
-        {
-
-                int c;
-
-
-
-                c = a + b;
-
-
-
-                return (c);
-
-        }
+	c = a + b;
+	return (c);
+}
