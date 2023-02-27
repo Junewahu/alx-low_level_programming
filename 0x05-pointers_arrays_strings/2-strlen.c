7c36f89770ac4986612cec8f625e6ab33e6ff9da
Junewahu <junewahu@gmail.com>
2023-02-27 14:30:34 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_strlen -returns the length of a string
+ *@s: string
+ *Return: returns length
+ */
+
+/*
+ * 1. Fx that returns an integer and takes pointer to a character as argument
+ * 2. Declare an integer variable count and initialize it to 0.
+ * 3. Declare an integer variable inc and initialize it to 0.
+ * 4. Use a for loop to iterate through the string s.
+ * 5. Increment inc by 1.
+ * 6. Return inc.
+*/
+/*
+ * Time Complexity: O(n)
+*/
+int _strlen(char *s)
+{
+	int count, inc;
+
+	inc = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+
+	return (inc);
+}
