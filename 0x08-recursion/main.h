d3719707e84f778046745a0375dd47e573bf7841
Junewahu <junewahu@gmail.com>
2023-03-09 16:20:33 -0800

    adv

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,12 +3,12 @@
 
 #include <unistd.h>
 /**
-*void prototypes(void)
-*int prototypes(int)
-*char prototypes(char) 
-*/
+ *void prototypes(void)
+ *int prototypes(int)
+ *char prototypes(char)
+ */
 void _puts_recursion(char *s);
- void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
